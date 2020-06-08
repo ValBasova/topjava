108f65c485c9713e7cf6f0a3a4bb01bcd15e9590
Valery Basova <basovavalery@gmail.com>
2020-06-08 17:15:48 -0400

    HW1 Optional

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -5,7 +5,8 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter inDATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
 
     public static String format(LocalDateTime date) {
         return date.format(DATE_FORMATTER);
@@ -13,5 +14,13 @@ public class TimeUtil {
 
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
+    }
+
+    public static String parseto(String date) {
+        return LocalDateTime.parse(date, inDATE_FORMATTER).format(DATE_FORMATTER);
+    }
+
+    public static LocalDateTime parse(String date) {
+        return LocalDateTime.parse(parseto(date), DATE_FORMATTER);
     }
 }
